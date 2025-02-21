bc96e2b0939839424b2fa8ece3bd2c8a943555ef
alisa2799 <alisa27guk@gmail.com>
2025-02-21 11:09:42 +0300

    second

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync'
+
+export const helloUser = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const askName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
